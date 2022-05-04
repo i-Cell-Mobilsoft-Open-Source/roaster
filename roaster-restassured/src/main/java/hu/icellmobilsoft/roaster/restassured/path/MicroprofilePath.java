daa7d18be24024538ba9b676ca9bec5a3f806723
Bucher Arnold <arnold.bucher@icellmobilsoft.hu>
2022-05-04 17:21:32 +0200

    #74 Health probe tipizálása

--- roaster-restassured/src/main/java/hu/icellmobilsoft/roaster/restassured/path/MicroprofilePath.java
+++ roaster-restassured/src/main/java/hu/icellmobilsoft/roaster/restassured/path/MicroprofilePath.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,12 +21,15 @@ package hu.icellmobilsoft.roaster.restassured.path;
 
 /**
  * Constants for mp endpoint paths
- * 
+ *
  * @author mark.petrenyi
  */
 public class MicroprofilePath {
 
     public static final String OPENAPI_PATH = "/openapi";
     public static final String HEALTH_PATH = "/health";
+    public static final String HEALTH_READY_PATH = "/health/ready";
+    public static final String HEALTH_LIVE_PATH = "/health/live";
+    public static final String HEALTH_STARTED_PATH = "/health/started";
 
 }
